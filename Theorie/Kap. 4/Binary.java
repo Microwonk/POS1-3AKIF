59a4f5e53ece725fab890a1b034e2f74dba2c317
Microwonk <nicolas.theo.frey@gmail.com>
2022-10-16 16:01:40 +0200

    1

--- Theorie/Kap. 4/Binary.java
+++ Theorie/Kap. 4/Binary.java
@@ -2,30 +2,47 @@ import java.util.Scanner;
 
 public class Binary {
     public static void main(String[] args) {
+
         Scanner scan = new Scanner(System.in);
 
-        char stellechar;
-        int stelle, stellen;
+        int stelle, stellen, i;
         long dezi;
         String binary;
 
-        System.out.println("---------Binärrechner---------");
-        System.out.print("Geben Sie eine Binäre Zahl ein: ");
-        binary = scan.next();
-        stellen = binary.length();
+        
         stelle = 0;
         dezi = 0;
+        i = 1;
 
-        while (stellen > 0) {
-            stellechar = binary.charAt(stelle);
-            if (Character.getNumericValue(stellechar) != 0) {
-                dezi = dezi + (int) Math.pow(2, stellen-1);
+        System.out.println("---------Binärrechner---------");
+        
+        while (i == 1) {
+            System.out.print("Geben Sie eine Binäre Zahl ein: ");
+            binary = scan.next();
+            stellen = binary.length();
+
+            if (binary.contains("1")&&binary.contains("0")) {
+
+                while (stellen > 0) {
+
+                    if (Character.getNumericValue(binary.charAt(stelle)) != 0) { 
+                        
+                        dezi = dezi + (int) Math.pow(2, stellen-1);
+                    }
+                    stellen--;
+                    stelle++;
+                    i = 0;
+                }
             }
-            stellen--;
-            stelle++;
+
+            else {
+                System.out.println("Keine Binärzahl, geben Sie noch einmal ein.");
+                System.out.println();
+            }
         }
+
         System.out.println();
-        System.out.println("Ihre Binärzahl " + binary + " lautet " + dezi + " im dezimal System.");
+        System.out.println("Ihre Binärzahl lautet " + dezi + " im dezimal System.");
 
         scan.close();
         return;
