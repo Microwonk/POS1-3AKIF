70495a1634451ff461ab4d41abc24360da0c2a07
Microwonk <nicolas.theo.frey@gmail.com>
2022-11-07 14:42:20 +0100

    a

--- Gailer/Kap. 18/Rechteck.java
+++ Gailer/Kap. 18/Rechteck.java
@@ -4,11 +4,11 @@ public class Rechteck {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int inputx1, inputy1, inputx2, inputy2;
-        int height = 1, length = 1, area = 1;
+        int height, length, area;
 
         System.out.println("CAD Programm");
 
-        while (area != 0 || length != 0 || height != 0) {
+        do {
             System.out.println("Erster Eckpunkt X:");
             inputx1 = scan.nextInt();
             System.out.println("Erster Eckpunkt Y:");
@@ -38,7 +38,10 @@ public class Rechteck {
             System.out.println("Breite: " + length);
             System.out.println("Höhe: " + height);
             System.out.println("Fläche: " + area);
-        }
+
+        } while (area != 0 || length != 0 || height != 0);
+
+
         scan.close();
     }
 }
