2a3d64ed655d8c94fb2b379793f7cb44c96bc25d
Microwonk <nicolas.theo.frey@gmail.com>
2022-12-07 10:07:10 +0100

    1

---
+++ Private Projects/FindingDeletables.java
@@ -0,0 +1,76 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class FindingDeletables {
+
+    static int ways = 0;
+    static int maxWays = 0;
+    static String maxDeletablePrime;
+
+    private static String removeByIndex(String primeNumber, int index) {
+        return new StringBuilder(primeNumber).deleteCharAt(index).toString();
+     }
+
+     private static boolean isPrime (String checkforPrime) {
+        BigInteger checkedPrime = new BigInteger(checkforPrime);
+        return checkedPrime.isProbablePrime(1);
+     }
+
+     private static void getDeletablePrime (String prime) {
+        int i = 0;
+        String temp;
+        
+        while (i < prime.length()) {
+        
+           if (prime.length() == 1) {
+                ways++;
+           }
+           else {
+              temp = removeByIndex(prime, i);
+              if (isPrime(temp)) {
+                  getDeletablePrime(temp);
+                }
+            }
+            i++;
+        }
+    }
+    
+    private static void iteratePrimes (long maxPrime) {
+        String prime;
+        long res = 13;
+        
+        while (res <= maxPrime) {
+            prime = Long.toString(res);
+            if (isPrime(prime)) {
+                getDeletablePrime(prime);
+            
+                if (ways > 0) {
+                    //Faulty code, sometimes the ways aren't counted correctly, please rely on my Theorie/Kap.5/DeletablePrimes Program to find out the ways for the given number(s)
+                    System.out.println(ways + " way(s) for: " + res);
+                }
+                if (ways > maxWays) {
+                    maxWays = ways;
+                    maxDeletablePrime = prime;
+                }
+            }
+            ways = 0;
+            res += 2;
+        }
+     }
+
+     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Up to what number should the deletable Primes be calculated?");
+        long restrictor = scan.nextLong();
+        scan.close();
+
+        iteratePrimes(restrictor);
+        System.out.println();
+        
+        //Faulty code, the Ways are not accurate enough (+- 1 - 2)
+
+        System.out.println("The maximum amount of ways in the range 0-" + restrictor + " was " + maxWays + " with the smallest number of " + maxDeletablePrime + "(the Ways may vary +- 1-2, please refer to DeletablePrimes.java to check the ways)");
+        
+     }
+}
