ef9de5fa569789ee0859d807c00395c4e862847f
Microwonk <nicolas.theo.frey@gmail.com>
2022-12-22 20:14:07 +0100

    Turorials

---
+++ Tutorials/Interfaces_Abstraction/Animal.java
@@ -0,0 +1,17 @@
+public abstract class Animal {
+    public String picture;
+    protected String name;
+    protected int age;
+
+    public Animal(String picture, String name, int age) {
+        this.picture = picture;
+        this.name = name;
+        this.age = age;
+    }
+
+    public void birthday() {
+        this.age++;
+    }
+
+    public abstract void makeSound();
+}
---
+++ Tutorials/Interfaces_Abstraction/Main.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        /* INTERFACES AND ABSTRACT */
+
+        Dog bengie = new Dog("bengie.png", "Bengie", 7);
+        Dog gracie = new Dog("gracie.png", "Gracie", 5);
+
+        bengie.birthday();
+
+        Cat whiskers = new Cat("whiskers.png", "Whiskers", 4);
+
+        bengie.makeSound();
+        whiskers.makeSound();
+
+        List<Animal> animals = new ArrayList<>();
+        animals.add(bengie);
+        animals.add(gracie);
+        animals.add(whiskers);
+
+        animals.get(1).makeSound();
+
+        Animal animal = new Dog("jenny.png", "Jenny", 12);
+        animal.makeSound();
+
+        // Animal animal1 = new Animal();
+
+        List<IPettable> pets = new ArrayList<>();
+        pets.add(bengie);
+        pets.add(whiskers);
+        pets.add(new Bird("yoshi.png", "Yoshi", 12));
+
+        for(IPettable pet : pets) {
+            pet.pet();
+        }
+
+        // IFly flying = (IFly)whiskers;
+        // flying.fly();
+
+    }
+}
