31dfb2c66dd22105ebc40d7f818570654483695b
Microwonk <nicolas.theo.frey@gmail.com>
2023-01-10 13:17:34 +0100

    Removing games and putting into other Repos

--- POS1_Spiel_Projekt/Connect_4/Connect_4/src/net/mikrowonk/connect_4_noArrays/Main.java
+++
@@ -1,8 +0,0 @@
-package net.mikrowonk.connect_4_noArrays;
-
-public class Main {
-    public static Game game = new Game();
-    public static void main(String[] args) {
-        game.startGame();
-    }
-}
