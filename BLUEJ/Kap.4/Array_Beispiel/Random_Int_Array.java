7fd2817d91225783f0920a3a5b519867d708dcf8
Microwonk <nicolas.theo.frey@gmail.com>
2023-01-10 16:12:50 +0100

    add fancy tolls

--- BLUEJ/Kap.4/Array_Beispiel/Random_Int_Array.java
+++ BLUEJ/Kap.4/Array_Beispiel/Random_Int_Array.java
@@ -10,7 +10,7 @@ public class Random_Int_Array {
 
     public void fillArray() {
         for (int i = 0; i < this.zahlen.length; i++) {
-            this.zahlen[i] = new Random().nextInt(1000) + 1;
+            this.zahlen[i] = new Random().nextInt(1, 1000);
         }
     }
 
@@ -19,10 +19,21 @@ public class Random_Int_Array {
         for (int zahl : zahlen) {
             if (i % 10 == 0) {
                 System.out.println();
+                System.out.println("----------------------------------------");
             }
-            System.out.print(zahl + " ");
+            System.out.print(calcSpaces(zahl) + zahl + "|");
             i++;
         }
         System.out.println();
+    }
+
+    public String calcSpaces(int zahl) {
+        if (Integer.toString(zahl).length() == 1) {
+            return "00";
+        }
+        else if(Integer.toString(zahl).length() == 2) {
+            return "0";
+        }
+        return "";
     }
 }
